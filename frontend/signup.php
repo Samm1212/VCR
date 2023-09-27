40966929cd11a59de7f011f3770ff9be4a73071e
Samm1212 <sam.fnd2@gmail.com>
2023-09-27 02:10:56 -0400

    Final Push

--- frontend/signup.php
+++ frontend/signup.php
@@ -8,8 +8,9 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
 
-    <body>
-    <div class="container-fluid border border-dark d-flex justify-content-center pt-4" style="width: fit-content;">
+    <body style="background-color: #89CFF0;">
+    <div class="pt-3"></div>
+    <div class="container-fluid border rounded border-dark d-flex justify-content-center p-4" style="width: fit-content; background-color:honeydew; padding: 0.3em;">
             <div style="width: 200px;">
             <form action="register.php" method="post">
                 <h2>SIGN UP</h2>
